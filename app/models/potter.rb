5974c89a1edbb0854e33650a3f40ded1e74fce3e
Javier Aguilar <jvr.agx@gmail.com>
2020-05-19 10:47:15 -0600

    Add method to list members of order of phoenix from a given house

--- app/models/potter.rb
+++ app/models/potter.rb
@@ -1,7 +1,13 @@
 module Potter
-  def self.list_houses
-    data = PotterService.new.list_houses
-    binding.pry
-    data.map { |repo| House.new(repo[:name], repo[:html_url]) }
+  def self.list_house_members_in_oof(house)
+    data = PotterService.new.list_characters_by_house_and_in_oof(house)
+    data.map do |member|
+      info = { member: member[:_id],
+               name: member[:name],
+               role: member[:role],
+               patronus: member[:patronus],
+               house: member[:house] }
+      Member.new(info)
+    end
   end
 end
