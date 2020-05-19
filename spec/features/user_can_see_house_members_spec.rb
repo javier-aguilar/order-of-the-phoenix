c94043daca0a835ecb77f5ef73f6317a6c3a6d2e
Javier Aguilar <jvr.agx@gmail.com>
2020-05-19 10:48:52 -0600

    Add spec test for search/index

--- spec/features/user_can_see_house_members_spec.rb
+++ spec/features/user_can_see_house_members_spec.rb
@@ -8,15 +8,38 @@ RSpec.describe "As a user" do
 
       expect(current_path).to eq search_path
 
-      expect(page).to eq(21)
-      # Then I should see the total number of the order of the phoenix members that belong to Gryffindor. (i.e. 21)
-      # And I should see a list with the detailed information for the 21 members of the Order of the Phoenix for house Gryffindor.
-      # ​
-      # And for each of the members I should see:
-      # - The name of the member
-      # - The members role (if it exists)
-      # - The house the member belongs to
-      # - The Patronus of the member (if it exists)
-      # ```
+      within('.total-members') do
+        expect(page).to have_content "21"
+      end
+
+      within('.members') do
+        expect(page.all('li')[0]).to have_content("Sirius Black")
+        expect(page.all('li')[1]).to have_content("Role:")
+        expect(page.all('li')[2]).to have_content("House: Gryffindor")
+        expect(page.all('li')[3]).to have_content("Patronus:")
+      end
+      within('.members') do
+        expect(page.all('li')[4]).to have_content("Aberforth Dumbledore")
+        expect(page.all('li')[5]).to have_content("Role: Owner, Hog's Head Inn")
+        expect(page.all('li')[6]).to have_content("House: Gryffindor")
+        expect(page.all('li')[7]).to have_content("Patronus: Goat")
+      end
+
+      visit root_path
+      select "Slytherin"
+      click_button "Search For Members"
+
+      expect(current_path).to eq search_path
+
+      within('.total-members') do
+        expect(page).to have_content "1"
+      end
+
+      within('.members') do
+        expect(page.all('li')[0]).to have_content("Severus Snape")
+        expect(page.all('li')[1]).to have_content("Role: Professor, Potions")
+        expect(page.all('li')[2]).to have_content("House: Slytherin")
+        expect(page.all('li')[3]).to have_content("Patronus: Doe")
+      end
     end
 end
